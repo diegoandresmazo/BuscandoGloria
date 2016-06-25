aadaf27006c6c411d798d9414b0928685c0dde75
Diego Andrés <diegoandresmazo@gmail.com>
2016-06-24 19:32:39 -0500

    Initial commit

---
+++ app/src/main/java/com/diegomazoapp/buscandogloria/Login.java
@@ -0,0 +1,25 @@
+package com.diegomazoapp.buscandogloria;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
